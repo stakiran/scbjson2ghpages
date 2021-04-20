a826239ab78265be887ee5e0e17025795143ca4c
stakiran <stakiran@users.noreply.github.com>
2021-04-21 08:03:29 +0900

    set debugprint to blocks.

--- test_pagetest.py
+++ test_pagetest.py
@@ -73,13 +73,13 @@ class TestModer(unittest.TestCase):
         self._test_onepage('image')
 
     def test_codeblock(self):
+        LIB.DP_scb_to_markdown_in_line = True
         self._test_onepage('codeblock')
+        LIB.DP_scb_to_markdown_in_line = False
 
     def test_table(self):
         LIB.DP_scb_to_markdown_in_line = True
-
         self._test_onepage('table')
-
         LIB.DP_scb_to_markdown_in_line = False
 
 if __name__ == '__main__':
