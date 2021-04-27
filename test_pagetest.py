666004693bd08d395f38458ad434b90c42844063
stakiran <stakiran@users.noreply.github.com>
2021-04-28 07:08:42 +0900

    debugoff

--- test_pagetest.py
+++ test_pagetest.py
@@ -81,7 +81,6 @@ class TestModer(unittest.TestCase):
         self._test_onepage('table')
 
     def test_nestedblock(self):
-        LIB.DP_convert_step2_after_append = True
         self._test_onepage('nested_block')
 
 if __name__ == '__main__':
