79d00a670ecda96d0de25002dfdf817f9dd98a70
stakiran <stakiran@users.noreply.github.com>
2021-05-05 09:13:31 +0900

    tweak テストデータの先頭スペースをなくす

--- test_scbjson2ghpages.py
+++ test_scbjson2ghpages.py
@@ -7,22 +7,24 @@ import scbjson2ghpages as main
 
 testdata_various_link = '''
 o
- [リンク]
- [リンク]と[リンク]
- [スペースを ふくむページへのリンク]
- [存在しないリンク] これは後続の「当該ページ名を持つページが実際に存在するか判定」で弾く
- #hash1 は問題なく #hash2 #hash2 検出されるはず #hash3
+[リンク]
+[リンク]と[リンク]
+[スペースを ふくむページへのリンク]
+[存在しないリンク] これは後続の「当該ページ名を持つページが実際に存在するか判定」で弾く
+#hash1 は問題なく #hash2 #hash2 検出されるはず #hash3
+
 x
- `[りんく]`
- [* 太字]
- [**** 太字]
- [- 斜体]
- [/sta]
- [http://www.google.co.jp google(text後)]
- [google(text前) http://www.google.co.jp]
- [https://gyazo.com/505861e8a5c21ae87eb972c4affd8841]
- [sta.icon]
- [sta.icon*3]
+`[りんく]`
+[* 太字]
+[**** 太字]
+[- 斜体]
+[/sta]
+[http://www.google.co.jp google(text後)]
+[google(text前) http://www.google.co.jp]
+[https://gyazo.com/505861e8a5c21ae87eb972c4affd8841]
+[sta.icon]
+[sta.icon*3]
+
 end'''
 
 class TestLinkContruction(unittest.TestCase):
